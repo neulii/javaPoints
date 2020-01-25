bcbda139e4208bbd02fef6b60467f18f0b963f58
neulii <neulii@gmx.at>
2020-01-25 10:51:35 +0100

    super

--- src/javaPoints/NewGameWindow.java
+++ src/javaPoints/NewGameWindow.java
@@ -2,11 +2,14 @@ package javaPoints;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class NewGameWindow {
 
     private JDialog window;
     private MainWindow mainWindow;
+    private JButton okButton;
+    private Component allWindowComponents[];
 
     public NewGameWindow(MainWindow mainWindow){
 
@@ -17,11 +20,33 @@ public class NewGameWindow {
         window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         window.setLocationRelativeTo(mainWindow.getFrame());
 
+        window.setLayout(null);
+
+
+        okButton = new JButton("test");
+        window.add(okButton);
+
+
+
+
 
 
     }
 
     public void setVisible(boolean visible){
         window.setVisible(visible);
+
+    }
+
+    public void addActionListener(ActionListener listener){
+
+        allWindowComponents = window.getContentPane().getComponents();
+
+        for(int i = 0; i<allWindowComponents.length; i++){
+            //TODO allWindowComponents[i].a
+
+        }
+
+
     }
 }
